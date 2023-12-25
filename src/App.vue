f90d2e1234f4e38793d8b3552d31505642041f99
Fitriawan Arya Nugraha <nugrahaaryaa76@gmail.com>
2023-12-25 16:46:59 +0700

    tambah styling card

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 
 <style>
 @import './assets/style/page.css';
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
 </style>
 
 <script>
--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -5,15 +5,26 @@
     <div v-if="loading">Loading...</div>
     <div v-else>
       <div v-for="product in displayedProducts" :key="product.id" class="product-card">
-        <img :src="product.image" :alt="product.title" />
-        <div class="product-info">
-            <h3>{{ product.title }}</h3>
+        <div class="content">
+          <div class="image">
+            <img :src="product.image" :alt="product.title" />
+          </div>
+          <div class="text">
+            <h2>{{ product.title }}</h2>
             <p>{{ product.category }}</p>
+            <hr />
             <p>{{ product.description }}</p>
-            <p>Harga: ${{ product.price }}</p>
+          </div>
+        </div>
+        <div class="text2">
+          <hr />
+          <h2 class="price">${{ product.price }}</h2>
+          <div class="btn">
+            <button class="btn-buy">Buy now</button>
+            <button class="btn-next" @click="nextProduct">Next product</button>
+          </div>
         </div>
       </div>
-      <button @click="nextProduct">Next Product</button>
     </div>
   </div>
 </template>
