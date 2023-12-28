41d4a884de37d3200392d48854e536d250ef61f5
Fitriawan Arya Nugraha <nugrahaaryaa76@gmail.com>
2023-12-28 20:59:21 +0700

    benerin tampilan

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
     <div v-if="loading">
-      <div class="loading-spinner"></div>
+      <div class="view">
+        <div class="loading-spinner"></div>
+      </div>
     </div>
 
     <div v-else>
-      <div
-        v-if="displayedProducts.length > 0 && !category.unavailable"
-        :class="getBackgroundClass()"
-      >
+      <div v-if="displayedProducts.length > 0 && !category.unavailable" :class="getBackgroundClass()">
         <div class="container">
           <div class="product-card">
+            <div class="image">
+              <img :src="displayedProducts[0].image" :alt="displayedProducts[0].title" />
+            </div>
             <div class="content">
-              <div class="image">
-                <img :src="displayedProducts[0].image" :alt="displayedProducts[0].title" />
-              </div>
               <div class="text">
                 <h2 :style="{ color: h2Color }">{{ displayedProducts[0].title }}</h2>
                 <div class="product-info">
@@ -26,43 +25,32 @@
                       {{ displayedProducts[0].rating.rate }}/5
                     </div>
                     <div class="circle-container">
-                      <div
-                        v-for="i in 5"
-                        :key="i"
-                        class="circle"
-                        :class="{
+                      <div v-for="i in 5" :key="i" class="circle" :class="{
                           'filled': i <= Math.floor(displayedProducts[0].rating.rate),
                           'circle-woman-filled': i <= Math.floor(displayedProducts[0].rating.rate) && isWomenCategory,
                           'circle-man-filled': i <= Math.floor(displayedProducts[0].rating.rate) && isMenCategory
-                        }"
-                      ></div>
+                        }"></div>
                     </div>
                   </div>
                 </div>
-                <hr />
+                <hr class="garisatas" />
                 <div class="ProdDescription">{{ displayedProducts[0].description }}</div>
+              </div>
+              <div class="text2">
+                <hr class="garisbawah" />
+                <h2 class="price">${{ displayedProducts[0].price }}</h2>
+                <div class="btn">
+                  <button class="btn-buy" :class="{ 'btn-men': isMenCategory, 'btn-women': isWomenCategory }">
+                    Buy now
+                  </button>
+                  <button class="btn-next" @click="nextProduct"
+                    :class="{ 'btn-men': isMenCategory, 'btn-women': isWomenCategory }">
+                    Next product
+                  </button>
+                </div>
               </div>
             </div>
 
-            <div class="text2">
-              <hr />
-              <h2 class="price">${{ displayedProducts[0].price }}</h2>
-              <div class="btn">
-                <button
-                  class="btn-buy"
-                  :class="{ 'btn-men': isMenCategory, 'btn-women': isWomenCategory }"
-                >
-                  Buy now
-                </button>
-                <button
-                  class="btn-next"
-                  @click="nextProduct"
-                  :class="{ 'btn-men': isMenCategory, 'btn-women': isWomenCategory }"
-                >
-                  Next product
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -81,75 +69,74 @@
 
 
 <script>
-export default {
-  data() {
-    return {
-      displayedProducts: [],
-      loading: true,
-      currentIndex: 1,
-      maxIndex: 20,
-      category: '',
-    };
-  },
-  computed: {
-    isMenCategory() {
-      return (
-        this.displayedProducts.length > 0 &&
-        this.displayedProducts[0].category === "men's clothing"
-      );
+  export default {
+    data() {
+      return {
+        displayedProducts: [],
+        loading: true,
+        currentIndex: 1,
+        maxIndex: 20,
+        category: '',
+      };
     },
-    isWomenCategory() {
-      return (
-        this.displayedProducts.length > 0 &&
-        this.displayedProducts[0].category === "women's clothing"
-      );
+    computed: {
+      isMenCategory() {
+        return (
+          this.displayedProducts.length > 0 &&
+          this.displayedProducts[0].category === "men's clothing"
+        );
+      },
+      isWomenCategory() {
+        return (
+          this.displayedProducts.length > 0 &&
+          this.displayedProducts[0].category === "women's clothing"
+        );
+      },
+      h2Color() {
+        return this.isMenCategory ? '#002772' : (this.isWomenCategory ? '#720060' : '');
+      },
     },
-    h2Color() {
-      return this.isMenCategory ? '#002772' : (this.isWomenCategory ? '#720060' : '');
-    },
-  },
-  mounted() {
-    this.fetchProducts();
-  },
-  methods: {
-    fetchProducts() {
-      this.loading = true;
-      fetch(`https://fakestoreapi.com/products/${this.currentIndex}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (
-            data.category === "men's clothing" ||
-            data.category === "women's clothing"
-          ) {
-            this.displayedProducts = [data];
-          } else {
-            this.displayedProducts = [];
-          }
-          this.loading = false;
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-          this.loading = false;
-        });
-    },
-    nextProduct() {
-      this.currentIndex = (this.currentIndex % this.maxIndex) + 1;
+    mounted() {
       this.fetchProducts();
     },
-    getBackgroundClass() {
-      const category =
-        this.displayedProducts.length > 0
-          ? this.displayedProducts[0].category
-          : '';
-      if (category === "men's clothing") {
-        return "BackgroundMan";
-      } else if (category === "women's clothing") {
-        return "BackgroundWoman";
-      } else {
-        return "BackgroundUnavailable";
-      }
+    methods: {
+      fetchProducts() {
+        this.loading = true;
+        fetch(`https://fakestoreapi.com/products/${this.currentIndex}`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (
+              data.category === "men's clothing" ||
+              data.category === "women's clothing"
+            ) {
+              this.displayedProducts = [data];
+            } else {
+              this.displayedProducts = [];
+            }
+            this.loading = false;
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+            this.loading = false;
+          });
+      },
+      nextProduct() {
+        this.currentIndex = (this.currentIndex % this.maxIndex) + 1;
+        this.fetchProducts();
+      },
+      getBackgroundClass() {
+        const category =
+          this.displayedProducts.length > 0
+            ? this.displayedProducts[0].category
+            : '';
+        if (category === "men's clothing") {
+          return "BackgroundMan";
+        } else if (category === "women's clothing") {
+          return "BackgroundWoman";
+        } else {
+          return "BackgroundUnavailable";
+        }
+      },
     },
-  },
-};
+  };
 </script>
-
